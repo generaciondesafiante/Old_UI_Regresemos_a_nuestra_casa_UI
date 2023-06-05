1b1d80a7fee98bad7a3fac6c7e0f21a3edd09b03
DanielMayo <daniielmayo.mayo@gmail.com>
2023-06-05 09:39:03 -0500

    modificacion de estilo componente learningpahttitleclass

--- src/components/molecules/LearningPahtTitleClass/LearningPathTitleClass.jsx
+++ src/components/molecules/LearningPahtTitleClass/LearningPathTitleClass.jsx
@@ -1,15 +1,13 @@
 import './LearningPathTitleClass.css';
 export const LearningPathTitleClass = ({ descriptionData }) => {
   return (
-    <>
-      <div className="classRoomInfo-container">
-        <p className="classRoomInfo-topic">{descriptionData.courseName}</p>
-        <div className="classRoomInfo-line"></div>
-        <h1 className="classRoomInfo-title">{descriptionData.lessonName}</h1>
-        <div className="classRoomInfo-subcontent">
-          <p>{descriptionData.lessonDescription}</p>
-        </div>
+    <div className="classRoomInfo-container">
+      <p className="classRoomInfo-topic">{descriptionData.courseName}</p>
+      <div className="classRoomInfo-line"></div>
+      <h2 className="classRoomInfo-title">{descriptionData.lessonName}</h2>
+      <div className="classRoomInfo-subcontent">
+        <p>{descriptionData.lessonDescription}</p>
       </div>
-    </>
+    </div>
   );
 };
