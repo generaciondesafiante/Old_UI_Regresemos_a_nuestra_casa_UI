839952f4f02f4506b72fdf42befeb648bad93c78
Abdul Florez <71296984+abdulflorez@users.noreply.github.com>
2023-06-23 12:12:37 -0500

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,6 @@ import { Carousel } from '../../components/organims/Carousel/Carousel';
 import './Home.css';
 
 export const Home = () => {
-  //just to comment
   return (
     <>
       <Header />
