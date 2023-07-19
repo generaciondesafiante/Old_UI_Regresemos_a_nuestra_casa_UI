c1edc5d6c7755cba6734b3dfb1a952855c00048c
SthefaniaEcheverry <104586980+SthefaniaEcheverry@users.noreply.github.com>
2023-07-19 15:39:50 -0500

    Des 153 improvement for sidebar styles stept 1 adding background color acording to section selected (#44)
    * add background to button
    
    * fix-format

--- src/components/molecules/Sidebar/Sidebar.jsx
+++ src/components/molecules/Sidebar/Sidebar.jsx
@@ -12,38 +12,114 @@ import {
 import { PrivateRoutes, PublicRoutes } from '../../../models/routes';
 import { useAuthStore } from '../../../hooks/useAuthStore';
 import './Sidebar.css';
+import { useState } from 'react';
 
 export const Sidebar = () => {
+  const [isSelected, setIsSelected] = useState({
+    profile: false,
+    dashboard: true,
+    path: false,
+    resources: false,
+    favorites: false,
+  });
   const { startLogout, status } = useAuthStore();
 
   if (status !== 'authenticated') {
     return null;
   }
 
+  const handleSelectedOption = (optionSelected) => {
+    const state = {
+      profile: false,
+      dashboard: false,
+      path: false,
+      resources: false,
+      favorites: false,
+    };
+    setIsSelected({ ...state, ...optionSelected });
+    console.log(isSelected);
+  };
+
+  const updateClass = (condition) => {
+    return `${condition ? 'sidebar-sectionSelected' : ''}`;
+  };
+
   return (
     <div className="sidebar-container">
-      <Link to={PrivateRoutes.PROFILE}>
-        <Face className="sidebar-icon" />
+      <Link
+        onClick={() => handleSelectedOption({ profile: true })}
+        to={PrivateRoutes.PROFILE}
+      >
+        <div
+          className={`sidebar-iconContainer ${updateClass(isSelected.profile)}`}
+        >
+          <Face className={`sidebar-icon ${updateClass(isSelected.profile)}`} />
+        </div>
       </Link>
       <div className="sidebar-content_center">
-        <Link to={PrivateRoutes.DASHBOARD}>
-          <Home className="sidebar-icon" />
+        <Link
+          onClick={() => handleSelectedOption({ dashboard: true })}
+          to={PrivateRoutes.DASHBOARD}
+        >
+          <div
+            className={`sidebar-iconContainer ${updateClass(
+              isSelected.dashboard
+            )}`}
+          >
+            <Home
+              className={`sidebar-icon ${updateClass(isSelected.dashboard)}`}
+            />
+          </div>
         </Link>
-        <Link to={PrivateRoutes.PATH}>
-          <Bookmark className="sidebar-icon" />
+        <Link
+          onClick={() => handleSelectedOption({ path: true })}
+          to={PrivateRoutes.PATH}
+        >
+          <div
+            className={`sidebar-iconContainer ${updateClass(isSelected.path)}`}
+          >
+            <Bookmark
+              className={`sidebar-icon ${updateClass(isSelected.path)}`}
+            />
+          </div>
         </Link>
-        <Link to={PrivateRoutes.RESOURCE}>
-          <Folder className="sidebar-icon" />
+        <Link
+          onClick={() => handleSelectedOption({ resources: true })}
+          to={PrivateRoutes.RESOURCE}
+        >
+          <div
+            className={`sidebar-iconContainer ${updateClass(
+              isSelected.resources
+            )}`}
+          >
+            <Folder
+              className={`sidebar-icon ${updateClass(isSelected.resources)}`}
+            />
+          </div>
         </Link>
-        <Link to={PrivateRoutes.FAVORITE}>
-          <Favorite className="sidebar-icon" />
+        <Link
+          onClick={() => handleSelectedOption({ favorites: true })}
+          to={PrivateRoutes.FAVORITE}
+        >
+          <div
+            className={`sidebar-iconContainer ${updateClass(
+              isSelected.favorites
+            )}`}
+          >
+            <Favorite
+              className={`sidebar-icon ${updateClass(isSelected.favorites)}`}
+            />
+          </div>
         </Link>
       </div>
-      <button onClick={startLogout} className="sidebar-icon">
-        <Link to={PublicRoutes.HOME} className="sidebar-icon">
+      <Link
+        onClick={() => handleSelectedOption({ dashboard: true })}
+        to={PublicRoutes.HOME}
+      >
+        <div onClick={startLogout} className="sidebar-iconContainer">
           <Logout className="sidebar-icon icon-exit" />
-        </Link>
-      </button>
+        </div>
+      </Link>
     </div>
   );
 };
