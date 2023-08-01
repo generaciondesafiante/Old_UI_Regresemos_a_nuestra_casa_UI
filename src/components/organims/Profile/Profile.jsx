ee0443a39f59558b6e07804a16485b4d77eca0a1
DanielMayo <daniielmayo.mayo@gmail.com>
2023-08-01 16:20:53 -0500

    change in the exit button to cancel

--- src/components/organims/Profile/Profile.jsx
+++ src/components/organims/Profile/Profile.jsx
@@ -184,7 +184,7 @@ export const Profile = () => {
                 onClick={() => setIsEditing(false)}
                 className="profile-user_changeInfo_btn goOut-profile"
               >
-                salir
+                Cancelar
               </button>
             </div>
           ) : (
