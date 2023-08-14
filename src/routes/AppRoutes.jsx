fc8c5d656fcb24268bd006d433a315ac6778cf33
DanielMayo <daniielmayo.mayo@gmail.com>
2023-08-14 12:02:45 -0500

    resolve conversation FETCH API CHANGE

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -37,7 +37,7 @@ export const AppRoutes = () => {
 
   useEffect(() => {
     if (coursesData[0].content.length === 0) {
-      fetch('http://localhost:8080/api/course/coursedata')
+      fetch('https://regresemos-cms.herokuapp.com/api/course/coursedata')
         .then((response) => response.json())
         .then((data) => {
           setCoursesData(data);
