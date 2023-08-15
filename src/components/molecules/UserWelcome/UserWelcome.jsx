f5f9772cff58c11d27810cd87d7c63c114c332f2
Sthefania Echeverry Orozco <sthefaniaecheverry@gmail.com>
2023-08-15 07:32:54 -0500

    Executing fix-format

--- src/components/molecules/UserWelcome/UserWelcome.jsx
+++ src/components/molecules/UserWelcome/UserWelcome.jsx
@@ -66,11 +66,6 @@ export const UserWelcome = () => {
             src="https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
             alt=""
           />
-           <img
-            className="dashboard-studyMaterial"
-            src="https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
-            alt=""
-          />
           <img
             className="dashboard-studyMaterial"
             src="https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
@@ -86,7 +81,12 @@ export const UserWelcome = () => {
             src="https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
             alt=""
           />
-           <img
+          <img
+            className="dashboard-studyMaterial"
+            src="https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+            alt=""
+          />
+          <img
             className="dashboard-studyMaterial"
             src="https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
             alt=""
